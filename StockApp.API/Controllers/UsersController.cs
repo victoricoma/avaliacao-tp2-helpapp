2ecf412d1b4735284c547bd2ecf259d9c2a906d9
Pedro4Albuquerque <pedrosouza1001000@gmail.com>
2025-06-26 16:59:37 -0300

    test: adiciona testes unitários para UsersController

---
+++ StockApp.API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockApp.Application.DTOs;
+using StockApp.Application.Interfaces;
+
+namespace StockApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserRegisterDto user)
+        {
+            if (user == null)
+                return BadRequest();
+
+            var success = await _userService.RegisterAsync(user);
+            if (success)
+                return Ok();
+            else
+                return Conflict();
+        }
+    }
+}
