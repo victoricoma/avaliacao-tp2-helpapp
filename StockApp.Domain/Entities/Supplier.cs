44e90fc4aa4cb06cacd018ff46d6d4f5a1fe7f86
Pedro4Albuquerque <pedrosouza1001000@gmail.com>
2025-06-27 15:30:27 -0300

    feat: adiciona entidade Supplier com validações no domínio

---
+++ StockApp.Domain/Entities/Supplier.cs
@@ -0,0 +1,58 @@
+﻿using StockApp.Domain.Validation;
+
+namespace StockApp.Domain.Entities
+{
+    public class Supplier
+    {
+        #region Atributos
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string ContactEmail { get; set; }
+
+        public string PhoneNumber { get; set; }
+        #endregion
+
+        #region Construtores
+        public Supplier(string name, string contactEmail, string phoneNumber)
+        {
+
+            ValidateDomain(name, contactEmail, phoneNumber);
+
+        }
+
+        public Supplier(int id, string name, string contactEmail, string phoneNumber)
+        {
+
+            DomainExceptionValidation.When(id < 0, "Invalid Id value.");
+            Id = id;
+
+            ValidateDomain(name, contactEmail, phoneNumber);
+
+        }
+        #endregion
+
+        #region Validação
+        private void ValidateDomain(string name, string contactEmail, string phoneNumber)
+        {
+            DomainExceptionValidation.When(string.IsNullOrWhiteSpace(name), "Name is required.");
+
+            DomainExceptionValidation.When(name.Length < 3, "Name too short. Minimum 3 characters.");
+
+
+            DomainExceptionValidation.When(string.IsNullOrWhiteSpace(contactEmail), "Contact email is required.");
+            DomainExceptionValidation.When(!contactEmail.Contains("@"), "Invalid email format.");
+
+
+            DomainExceptionValidation.When(string.IsNullOrWhiteSpace(phoneNumber), "Phone number is required.");
+            DomainExceptionValidation.When(phoneNumber.Length < 8, "Phone number too short.");
+
+            Name = name;
+
+            ContactEmail = contactEmail;
+
+            PhoneNumber = phoneNumber;
+        }
+        #endregion
+    }
+}
