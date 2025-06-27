964a4dbe48b96c6ce97161f4e8d9136e61646432
WillianSCorrea <willianwcorrea@gmail.com>
2025-06-27 19:16:24 -0300

    10 .Criação do Controlador TokenController

---
+++ StockApp.API/Controllers/TokenController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StockApp.Infra.Data.Identity;
+using StockApp.Application;
+using StockApp.Application.DTOs;
+
+namespace StockApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public TokenController(IAuthService authService)
+        {
+            _authService = authService;
+
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] UserLoginDTO userLoginDTO)
+        {
+         
+            var token = await _authService.AuthenticateAsync(userLoginDTO.Username, userLoginDTO.Password);
+            if (token == null)
+            {
+                return Unauthorized("Invalid username or password");
+            }
+            return Ok(token);
+        }
+    }
+}
