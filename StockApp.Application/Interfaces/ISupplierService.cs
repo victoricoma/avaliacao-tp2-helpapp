6cfb9c45c894e0c9ac64a355d5fa6a054a798b0e
Pedro4Albuquerque <pedrosouza1001000@gmail.com>
2025-06-27 16:36:14 -0300

    feat: criar SuppliersController (#76)

---
+++ StockApp.Application/Interfaces/ISupplierService.cs
@@ -0,0 +1,17 @@
+﻿using StockApp.Application.DTOs;
+
+namespace StockApp.Application.Interfaces
+{
+    public interface ISupplierService
+    {
+        Task<IEnumerable<SupplierDTO>> GetSuppliers();
+
+        Task<SupplierDTO> GetSupplierById(int? id);
+
+        Task Add(SupplierDTO supplierDto);
+
+        Task Update(SupplierDTO supplierDto);
+
+        Task Remove(int? id);
+    }
+}
