4d4667325cf31b21f415b902a32f347bfc1002ea
VitorBZS <bonilhazanattasilva@gmail.com>
2025-06-26 21:18:45 -0300

    Criação do UserRegisterDto

--- StockApp.Application/DTOs/UserRegisterDTO.cs
+++ StockApp.Application/DTOs/UserRegisterDTO.cs
@@ -1,9 +1,16 @@
-﻿namespace StockApp.Application.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockApp.Application.DTOs
 {
     public class UserRegisterDto
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string Role { get; set; }
+        [Required(ErrorMessage = "O campo username é obrigatório.")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "O campo senha é obrigatório.")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "O campo perfil é obrigatório.")]
+        public string? Role { get; set; }
     }
 }
