9c73ac282903f6d5ec933519b8652be218f0c490
WillianSCorrea <willianwcorrea@gmail.com>
2025-06-28 17:02:23 -0300

    #

--- StockApp.API/Program.cs
+++ StockApp.API/Program.cs
@@ -14,12 +14,8 @@ internal class Program
         // Add services to the container.
         builder.Services.AddInfrastructureAPI(builder.Configuration);
 
-        builder.Services.AddStackExchangeRedisCache(options =>
-        {
-            options.Configuration = builder.Configuration.GetConnectionString("Redis");
+        
 
-            options.InstanceName = "StockApp";
-        });
 
         builder.Services.AddControllers();
 
