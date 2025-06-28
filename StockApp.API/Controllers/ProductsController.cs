455850130149710907c616696639b0c829a2e35c
Vitaminazz <vitamina.twitu@gmail.com>
2025-06-28 14:10:21 -0300

    Criação do controller de product

---
+++ StockApp.API/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockApp.Application.DTOs;
+using StockApp.Application.Interfaces;
+
+namespace StockApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> Get()
+        {
+            var produtos = await _productService.GetProducts();
+            if (produtos == null)
+            {
+                return NotFound("Products not found");
+            }
+            return Ok(produtos);
+        }
+        [HttpGet("{id}", Name = "GetProduct")]
+        public async Task<ActionResult<ProductDTO>> Get(int id)
+        {
+            var produto = await _productService.GetProductById(id);
+            if (produto == null)
+            {
+                return NotFound("Product not found");
+            }
+            return Ok(produto);
+        }
+    }
+}
