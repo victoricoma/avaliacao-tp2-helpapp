6785cb0616c4c06a9ed86fc61e1981c15cb4412c
miriam-silva <miriam.correa@fatec.sp.gov.br>
2025-06-29 19:45:41 -0300

    feat(test): adiciona testes unitários para TokenController

---
+++ StockApp.Domain.Test/TestControllers/TokenControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using StockApp.API.Controllers;
+using StockApp.Application.DTOs;
+using Xunit;
+
+namespace StockApp.Domain.Test.TestControllers
+{
+    public class TokenControllerTests
+    {
+        [Fact]
+        public void Login_ValidCredentials_ReturnsToken()
+        {
+            var controller = new TokenController();
+
+            var loginDto = new UserLoginDto
+            {
+                UserName = "testuser321",
+                Password = "password123"
+            };
+
+            var result =  controller.Login(loginDto) as OkObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(200, result.StatusCode);
+            
+            var token = result.Value as TokenResponseDto;
+            Assert.NotNull(token);
+            Assert.Equal("fake-jwt-token", token.Token);
+        }
+
+        [Fact]
+        public void Login_InvalidCredentials_ReturnsUnauthorized()
+        {
+            var controller = new TokenController();
+
+            var loginDto = new UserLoginDto
+            {
+                UserName = "usuarioErrado",
+                Password = "senhaErrada"
+            };
+
+            var result = controller.Login(loginDto);
+
+            Assert.IsType<UnauthorizedResult>(result);
+        }
+    }
+}
