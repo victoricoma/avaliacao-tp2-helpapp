d366b7a9526d604e9616e943289f3b0f590d8e6e
Victor <victorsantosflx@gmail.com>
2025-06-28 21:27:32 -0300

    Atividade 62

---
+++ StockApp.Infra.Data/Identity/ClaimsAuthorizationHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using StockApp.Domain.Entities;
+
+namespace StockApp.Infra.Data.Identity
+{
+    public class ClaimsAuthorizationHandler : AuthorizationHandler<ClaimsAuthorizationRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ClaimsAuthorizationRequirement requirement)
+        {
+            if (context.User == null)
+            {
+                return Task.CompletedTask;
+            }
+
+            if (context.User.HasClaim(c => c.Type == 
+            requirement.ClaimType && c.Value == requirement.ClaimValue))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
