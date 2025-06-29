071846e6c8d33e24eab524a6d9868882f9064af5
WillianSCorrea <willianwcorrea@gmail.com>
2025-06-28 21:58:43 -0300

    27. Implementar Endpoint de Atualização em Massa

--- StockApp.Application/Interfaces/IProductService.cs
+++ StockApp.Application/Interfaces/IProductService.cs
@@ -15,5 +15,6 @@ namespace StockApp.Application.Interfaces
         Task Update(ProductDTO productDto);
         Task Remove(int? id);
         Task<IEnumerable<ProductDTO>>  EstoqueBaixo(int limiteEstoque);
+        Task BulkUpdateAsync(List<ProductDTO> productsDTO);
     }
 }
